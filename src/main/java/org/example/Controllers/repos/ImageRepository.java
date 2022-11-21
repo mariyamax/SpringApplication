fc1774d79d923a3344eb1f75375b3183866f8729
mariyamax <89139747+mariyamax@users.noreply.github.com>
2022-11-21 16:12:13 +0300

    Delete src/main directory

--- src/main/java/org/example/Controllers/repos/ImageRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.Controllers.repos;
-
-import org.example.Controllers.model.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image,Long> {
-    Image findByID(Long id);
-}
